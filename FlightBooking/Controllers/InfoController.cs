874350a3f7b81932fb4d08a0ba2a8189f3ee0ca5
Ayse Mandirali <ayse.maandirali@gmail.com>
2026-08-09 13:33:04 +0300

    Hakkımızda sayfası ekledim ve footer kurumsal bağlantılarını gerçekleştirdim
    Footer'daki "Keşfet" sütunundaki Hakkımızda, Ödüller, Kariyer, Basın
    Bülteni gibi bağlantılar ya ana sayfaya ya da asistan sayfasına gidip
    boşa çıkıyordu. Şirket tanıtımı, sayılarla özet, misyon-vizyon-değerler
    bölümlerinden oluşan gerçek bir Hakkımızda sayfası oluşturdum ve footer
    bağlantılarını bu sayfaya ve mevcut hizmet sayfalarına (araç, tur,
    transfer, uçuş durumu) yönlendirerek boşa giden bağlantıları giderdim.

--- FlightBooking/Controllers/InfoController.cs
+++ FlightBooking/Controllers/InfoController.cs
@@ -5,6 +5,7 @@ namespace FlightBooking.Controllers
     // Footer'daki kurumsal bilgi sayfaları (Kullanım Koşulları, Gizlilik, Çerez, Site Haritası).
     public class InfoController : Controller
     {
+        public IActionResult About() => View();
         public IActionResult Terms() => View();
         public IActionResult Privacy() => View();
         public IActionResult Cookies() => View();
